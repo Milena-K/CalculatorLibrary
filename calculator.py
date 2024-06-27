c3fd42599cfa8879700ac306cb921193654c7c9a
Milena Kukolj <mkukolj@protonmail.com>
2024-06-27 16:19:47 +0200

    Add multiply and divide functions

--- calculator.py
+++ calculator.py
@@ -1,6 +1,16 @@
-def add(x: float, y: float):
+def add(x: float, y: float) -> float:
     return x + y
 
 
-def subtract(x: float, y: float):
+def subtract(x: float, y: float) -> float:
     return x - y
+
+
+def multiply(x: float, y: float) -> float:
+    return x * y
+
+def divide(x: float, y: float) -> float | None:
+    try:
+        return x / y
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero")
--- test_calculator.py
+++ test_calculator.py
@@ -10,3 +10,9 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10, 10)
+
+    def test_division(self):
+        assert 1 == calculator.divide(10, 10)
